8cc08a140db1061f4086f1afc808309443d47a59
左岸 <3112203373@163.com>
2023-09-01 23:48:28 +0800

    feat: 表头逻辑ok

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import classNames from 'classnames';
 import { NativeProps, withNativeProps } from '@/utils/native-props';
 import { DownOutline } from 'antd-mobile-icons';
@@ -21,10 +21,19 @@ const defaultProps: TableProps = {
 
 export const Table: FC<TableProps> = p => {
     const props = mergeProps(defaultProps, p)
-    const { showHeader, columns, data } = props;
+    const { showHeader, columns, data, onSort } = props;
+
+    const [curData, setCurData] = useState(data);
 
     const handleSorterClick = (item: TableColumnsProps) => {
-
+        if (item?.sorter) {
+            onSort && onSort(item, data);
+            if (typeof item.sorter === "function") {
+                setCurData(curData.sort(item.sorter as (a: any, b: any) => number));
+            } else {
+                setCurData(item.sorter === 'default' ? curData.sort() : curData)
+            }
+        }
     }
 
     const headElement = (
@@ -36,6 +45,7 @@ export const Table: FC<TableProps> = p => {
                         className={classNames(`${classPrefix}-cell`, {
                             [`${classPrefix}-align-${item.align}`]: item.align ?? false
                         })}
+                        onClick={() => handleSorterClick(item)}
                     >
                         {item.title}
                         {item.sorter && (
